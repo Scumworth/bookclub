75e16bc426b762ec882b12b091928ca02554fadb
scumworth <danmcninch@gmail.com>
2017-11-30 15:06:29 -0500

    pre heroku commit

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -6,6 +6,7 @@ const Footer = () => (
     <div style = {{ fontSize: 10, marginTop: 50, padding: 20, textAlign: 'center' }}>
         <p>This app is built for FreeCodeCamp</p>
         <p><a href = "https://github.com/Scumworth/bookclub">Github</a></p>
+        <p>Built using the Google-Books API.</p>
     </div>
 );
 
